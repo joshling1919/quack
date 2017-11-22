99d7f354d2c2a82bd2542edea3f455333bfab60b
Joshua Ling <joshling1919@gmail.com>
2017-11-21 22:28:24 -0600

    Start auth pattern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private 
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token 
+    @current_user = user 
+  end
+
+  def logout 
+    current_user.reset_session_token! 
+    session[:session_token] = nil 
+    @current_user = nil 
+  end
 end
