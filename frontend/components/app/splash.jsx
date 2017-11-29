1114a95e3689ddd1084a5a01df895fb3b86112c3
Joshua Ling <joshling1919@gmail.com>
2017-11-29 00:44:58 -0800

    Add splash title and description

--- frontend/components/app/splash.jsx
+++ frontend/components/app/splash.jsx
@@ -10,8 +10,16 @@ const Splash = () => (
         </div>
       </div>
     </nav>
-    <div>
-      Where Work Happens
+    <div className="splash-container">
+      <div className="splash">
+        <h1 className="splash-header">Where Work Happens</h1>
+        <p className="splash-description">
+          When your team needs to kick off a project, hire a new employee, 
+          deploy some code, review a sales contract, finalize next year's budget,
+          measure A/B test, plan your next office opening, and more, Slack 
+          has you covered. 
+        </p>
+      </div>
     </div>
   </div>
 );
