0e30baf843cb8a394af3fb2fe6bc80fa497ce10e
Joshua Ling <joshling1919@gmail.com>
2017-12-04 22:44:14 -0800

    Finish splash page

--- frontend/components/app/splash.jsx
+++ frontend/components/app/splash.jsx
@@ -8,17 +8,30 @@ const Splash = () => (
           <i className="fa fa-slack" aria-hidden="true"/>
           <a>quack</a>
         </div>
+        <button className="demo">Demo</button>
       </div>
     </nav>
     <div className="splash-container">
       <div className="splash">
-        <h1 className="splash-header">Where Work Happens</h1>
-        <p className="splash-description">
-          When your team needs to kick off a project, hire a new employee, 
-          deploy some code, review a sales contract, finalize next year's budget,
-          measure A/B test, plan your next office opening, and more, Slack 
-          has you covered. 
-        </p>
+        <div className="splash-text">
+          <h1 className="splash-header">Where Work Happens</h1>
+          <p className="splash-description">
+            When your team needs to kick off a project, hire a new employee, 
+            deploy some code, review a sales contract, finalize next year's budget,
+            measure A/B test, plan your next office opening, and more, Slack 
+            has you covered. 
+          </p>
+          <div className="splash-auth">
+            <button>Get Started</button>
+            <p>Already using Quack? <a>Sign In.</a></p>
+          </div>
+        </div>
+        <div className="splash-image-container">
+          <img 
+            src="https://a.slack-edge.com/52353/marketing/img/home/home_illo.png"
+            srcSet="https://a.slack-edge.com/52353/marketing/img/home/home_illo.png 1x, https://a.slack-edge.com/52353/marketing/img/home/home_illo@2x.png 2x"
+          />
+        </div>
       </div>
     </div>
   </div>
