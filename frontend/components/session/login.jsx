7955d8313825f1880c6b7a82f0813f2dc926536d
Joshua Ling <joshling1919@gmail.com>
2017-12-08 00:36:45 -0800

    Change to border-box and style log in

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const LogIn = () => (
   <div className="auth-background">
     <div className="auth-container">
-      Log In
+      <h1>Sign in</h1>
+      <form className="auth-form">
+        <p>Enter your <strong>email address</strong> and <strong>password.</strong></p>
+        <input 
+          type="email" 
+          size={40}
+          name="email" 
+          placeholder="you@example.com" 
+        />
+        <input 
+          type="password"
+          name="password"
+          size={40}
+          placeholder="password"
+        />
+        <button>Sign In</button>
+      </form>
     </div>
   </div>
 );
