acc25e92601f67e0b453e0a9248923736af7e468
Joshua Ling <joshling1919@gmail.com>
2017-12-08 21:54:01 -0800

    Add and style sign up component

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -2,7 +2,44 @@ import React from 'react';
 
 const SignUp = () => (
   <div className="auth-background">
-    SIGN UP
+    <div className="auth-container">
+      <h1>Sign up</h1>
+      <form className="auth-form">
+        <p>
+          Enter your 
+          <strong> email address</strong>,
+          <strong> full name</strong>,
+          <strong> username</strong>
+          , and 
+          <strong> password.</strong>
+        </p>
+        <input
+          type="email"
+          size={40}
+          name="email"
+          placeholder="you@example.com"
+        />
+        <input
+          type="text"
+          size={40}
+          name="name"
+          placeholder="full name"
+        />
+        <input
+          type="text"
+          size={40}
+          name="username"
+          placeholder="username"
+        />
+        <input
+          type="password"
+          name="password"
+          size={40}
+          placeholder="password"
+        />
+        <button>Create Account</button>
+      </form>
+    </div>
   </div>
 );
 
