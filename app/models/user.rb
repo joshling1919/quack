02fb72c65631bfe5774a3c61d757271fc22d9dc4
Joshua Ling <joshling1919@gmail.com>
2017-12-12 00:41:02 -0800

    Add email column and debug rails auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
 
   attr_reader :password 
 
-  validates :username, :password_digest, :session_token, :full_name, presence: true 
-  validates :username, uniqueness: true 
+  validates :username, :password_digest, :session_token, :fullname, :email, presence: true 
+  validates :username, :email, uniqueness: true 
   validates :password, length: { minimum: 6 }, allow_nil: true 
 
   after_initialize :ensure_session_token 
@@ -12,6 +12,11 @@ class User < ApplicationRecord
     user = User.find_by(username: username)
     return nil unless user 
     user.has_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def has_password?(password)
